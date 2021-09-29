b81d36236f8e2dc708a969aa11b5f6f9e1694d0e
Boris <trefata@gmail.com>
2021-09-29 17:18:21 +0300

    added JS-Advanced.

---
+++ JS-Advanced/SyntaxFunctionsAndStatements/greatestCommonDivisor.js
@@ -0,0 +1,9 @@
+function greatestCommonDivisor(a, b) {
+    while (b != 0) {
+        const temp = b;
+        b = a % b;
+        a = temp
+    }
+    return a;
+}
+console.log((greatestCommonDivisor(15, 5)))
