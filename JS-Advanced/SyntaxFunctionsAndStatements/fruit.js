ccaf46264e7f6febf46088dd39aeaaf577a5390f
Boris <88730388+Boris-Apostolov@users.noreply.github.com>
2021-09-29 17:17:10 +0300

    Delete JS-Advanced/SyntaxFunctionsAndStatements directory

--- JS-Advanced/SyntaxFunctionsAndStatements/fruit.js
+++
@@ -1,7 +0,0 @@
-function fruits(fruit, weight, money) {
-    const weightInKg = (weight / 1000);
-    const moneyNeeded = (weight / 1000) * money;
-
-    console.log(`I need $${moneyNeeded.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruit}.`);
-}
-fruits('orange', 2500, 1.80)
--- JS-Advanced/SyntaxFunctionsAndStatements/greatestCommonDivisor.js
+++
@@ -1,9 +0,0 @@
-function greatestCommonDivisor(a, b) {
-    while (b != 0) {
-        const temp = b;
-        b = a % b;
-        a = temp
-    }
-    return a;
-}
-console.log((greatestCommonDivisor(15, 5)))
